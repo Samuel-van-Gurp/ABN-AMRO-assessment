81aeef904e32c5269d26905396a329391b4e1d0a
Samuel-van-Gurp <82167906+Samuel-van-Gurp@users.noreply.github.com>
2024-03-04 15:16:11 +0000

    remove small experimet files

--- test.py
+++
@@ -1,5 +0,0 @@
-from pyspark.sql import SparkSession
-# spark = SparkSession.builder.appName("costumer_data").getOrCreate()
-spark = SparkSession.builder.master("local").appName("costumer_data").getOrCreate()
-
-print("done")
