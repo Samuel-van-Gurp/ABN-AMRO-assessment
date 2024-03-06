5009ec8bd63fd54d20c4065dccd56b2102c51267
Samuel-van-Gurp <82167906+Samuel-van-Gurp@users.noreply.github.com>
2024-03-06 11:09:40 +0000

    Refactor load_and_clean_data function, add exception handling, and update function signatures

--- pipline.py
+++ pipline.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+
 spark = SparkSession.builder.appName("costumer_data").getOrCreate()
 
 def load_and_clean_data(path_to_data: str,
@@ -8,16 +9,22 @@ def load_and_clean_data(path_to_data: str,
     Function load a CSV file into Spark DataFrames and cleans the data, by dropping columns and renaming columns.
 
     :arg path_to_data: str
-        The file path to the dataset (in csv file).
+        The file path to the dataset (CSV file).
     :arg columns_to_drop: list, optional
-        A list of column names to drop from the first dataset. Default is [].
-    :arg column_rename: dict
-        A dictionary specifying column renaming for the first dataset. Default is {}.
- 
-    :return: Spark DataFrame
-        Cleaned DataFrame 
+        A list of column names to drop from the dataset. Default is [].
+    :arg column_rename: dict, optional
+        A dictionary specifying column renaming. Default is {}.
+    :arg infer_schema: bool, optional
+        Whether to infer the schema of the dataset. Default is True.
+    :return: pyspark.sql.DataFrame
+        Cleaned DataFrame
     """
-    df = spark.read.csv(path_to_data, header=True, inferSchema=True)
+    # Load data
+    
+    try:
+        df = spark.read.csv(path_to_data, header=True, inferSchema=True)
+    except Exception as e:
+        raise IOError(f"Error loading data: {e}")
     
     # Drop specified columns
     df = df.drop(*columns_to_drop)
@@ -32,13 +39,14 @@ def filter(df, filter_conditions={}):
     """
     function filters the DataFrame based on the specified conditions.
 
-    :arg df: Spark DataFrame
+    :arg df: pyspark.sql.DataFrame
+        The DataFrame to be filtered.
+    :arg filter_conditions: dict, optional
+        A dictionary specifying filtering conditions as column-value pairs.
+        Default is an empty dictionary.
 
-    :arg filter_conditions: dict
-        A dictionary specifying filtering conditions as column-value pairs. Default is None.
-
-    :return: Spark DataFrame
-        Joined DataFrame after applying filter conditions.
+    :return: pyspark.sql.DataFrame
+        Filtered DataFrame.
     """
     if filter_conditions:
         for column, values in filter_conditions.items():
@@ -57,7 +65,7 @@ def join(df1, df2, join_key, join_type='inner'):
     :arg join_key: str
         The column name to join the DataFrames on.
     :arg join_type: str, optional
-        The type of join to perform. Default is 'inner'.
+        The type of join to perform. Default is 'inner',  valid join types = ['inner', 'left', 'right', 'outer'].
 
     :return: Spark DataFrame
         Joined DataFrame after applying the join.
@@ -88,14 +96,41 @@ def main(data_path_one: str,
          filter_conditions_1 = {},
          filter_conditions_2 = {},
          join_key='client_identifier', 
-         join_type='inner'):
-    
+         join_type='inner') -> None:
+    '''	
+    Main function to load, clean, filter, join two data sets and save the result.
+
+    :arg data_path_one: str
+        File path to the first dataset.
+    :arg data_path_two: str
+        File path to the second dataset.
+    :arg data_path_out: str
+        File path to save the joined data.
+    :arg columns_to_drop1: list, optional
+        A list of column names to drop from the first dataset. Default is [].
+    :arg columns_to_drop2: list, optional
+        A list of column names to drop from the second dataset. Default is [].
+    :arg column_rename_1: dict, optional
+        A dictionary specifying column renaming for the first dataset. Default is {}.
+    :arg column_rename_2: dict, optional
+        A dictionary specifying column renaming for the second dataset. Default is {}.
+    :arg filter_conditions_1: dict, optional
+        A dictionary specifying filtering conditions for the first dataset. Default is {}.
+    :arg filter_conditions_2: dict, optional
+        A dictionary specifying filtering conditions for the second dataset. Default is {}.
+    :arg join_key: str, optional
+        The column name to join the datasets on. Default is 'client_identifier'.
+    :arg join_type: str, optional
+        The type of join to perform. Default is 'inner'.
+
+    :return: None
+    '''
     # Load and clean the data
     df1 = load_and_clean_data(data_path_one, 
                             columns_to_drop=columns_to_drop1, 
                             column_rename=column_rename_1)
     df2 = load_and_clean_data(data_path_two, 
-                        columns_to_drop=columns_to_drop1, 
+                        columns_to_drop=columns_to_drop2, 
                         column_rename=column_rename_2)
     
     # filter the data in data_set one )
@@ -112,7 +147,7 @@ def main(data_path_one: str,
 if __name__ == "__main__":
     main('dataset_one.csv', 'dataset_two.csv', 'client_data/joined_data',
          columns_to_drop1=['first_name', 'last_name'],
-         columns_to_drop2=['client_identifier'],
+         columns_to_drop2=['cc_n'],
          column_rename_1={'id': 'client_identifier'},
          column_rename_2={'id': 'client_identifier', 'btc_a': 'bitcoin_address', 'cc_t': 'credit_card_type'},
          filter_conditions_1={'country': ['United Kingdom', 'Netherlands']},
